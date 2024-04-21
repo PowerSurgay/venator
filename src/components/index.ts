8e5351a92c848a456f1206f801bf34c3b57ed503
PowerSurgay <PaverSurgay@yandex.ru>
2024-04-21 15:10:34 +0300

    fix: grid

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,5 @@ export { Popup } from './Popup/Popup';
 export { ProductCard } from './ProductCard/ProductCard';
 export { Search } from './Search/Search';
 export { SidebarMenu } from './SidebarMenu/SidebarMenu';
+export { Grid } from './Grid/Grid';
 export { NoPage } from './NoPage/NoPage';
